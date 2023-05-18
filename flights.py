af4f121367c94a9fa7aafb72a39ea0120c9b8242
amir zarabadipour <amirzarabadipour@gmail.com>
2023-05-18 16:55:27 +0200

    flights.csv added

--- flights.py
+++ flights.py
@@ -4,7 +4,7 @@ from os.path import dirname, join
 import pandas as pd
 from hist import hist_tab
 
-data = pd.read_csv('flights.csv', index_col=0).dropna()
+data = pd.read_csv("flights.csv", index_col=0).dropna()
 
 tab_hist = hist_tab(data)
 tabs = Tabs(tabs=[tab_hist])
